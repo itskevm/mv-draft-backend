c806cda855294c3a6220248ebb5b755fcd6a7abd
itskevm <kevmgaming@gmail.com>
2021-01-20 04:40:07 -0700

    fixing model

--- src/movies/models.py
+++ src/movies/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Movie(models.Model):
     title = models.CharField(max_length=120)
     author = models.CharField(max_length=64)
     content = models.TextField()
+    # created_on = models.DateField("Created on", default=timezone.now)
 
     def __str__(self):
         return self.title
